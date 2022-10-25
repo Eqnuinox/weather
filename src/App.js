7b1316f5adad2c14e2ad3b56777c660f0e7895b3
Eqnuinox <eq.avlasenko.nikit@gmail.com>
2022-10-25 16:11:09 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const Header = ({startDecoding}) => {
+
+      const [location, setLocation] = useState('Минск')
+      const [currentLoc, setCurrentLoc] = useState()
+      const [lat, setLat] = useState()
+      const [long, setLong] = useState()
+  
+      const handleSearchChange = (event) => setLocation(event.target.value);
+      
+      // useEffect(() => {
+      //     navigator.geolocation.getCurrentPosition(function(position) {
+      //       setLat(position.coords.latitude);
+      //       setLong(position.coords.longitude);
+      //     });
+          
+
+      //     console.log("Shirota:", lat)
+      //     console.log("Dolgota:", long)
+
+      // }, [lat, long]);
+  
+      return (
+          <div>
+              <div>
+                  <input
+                      type=""
+                      value={location}
+                      placeholder="Search..."
+                      onChange={handleSearchChange}
+                  />
+                  <button onClick={() => startDecoding(location)}>
+                      <i>search</i>
+                  </button>
+                  <button onClick={() => startDecoding("Minsk")}>
+                      <i>Minsk</i>
+                  </button>
+                  <button onClick={() => startDecoding("Moscow")}>
+                      <i>Moscow</i>
+                  </button>
+                  <button onClick={() => startDecoding('Bratislava')}>
+                      <i>Bratislava</i>
+                  </button>
+              </div>
+          </div>
+      )
+  }
+  
+  export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useEffect, useState } from 'react';
+import Header from './App';
+import { decodingApiRequest, weatherApiRequest } from './utils';
+
+const App = () => {
+
+  const [decodingResults, setDecodingResults] = useState('')
+  const [searchResults, setSearchResults] = useState()
+
+
+  const updateDecoding = async (text) => {
+    const response = await decodingApiRequest(text);
+    console.log("GOVNO = ",response) 
+    setDecodingResults(response)
+    console.log("GOVNOSHE",decodingResults)
+
+  }
+
+  useEffect((decodingResults) => {
+    const response = decodingResults.map((location) => weatherApiRequest(location))
+    setSearchResults(response);
+
+  }, [decodingResults])
+
+  return (
+    <>
+      <div>
+        <Header startDecoding={updateDecoding} />
+      </div>
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
